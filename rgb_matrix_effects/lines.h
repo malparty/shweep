b098f991be9d0a2aaac46664c10903c71fb0cca3
Xavier MALPARTY <xavier@nimblehq.co>
2024-05-16 20:16:45 +0700

    Add the two faces annimation

--- rgb_matrix_effects/lines.h
+++ rgb_matrix_effects/lines.h
@@ -1,11 +1,11 @@
-RGB_MATRIX_EFFECT(TWO_FACES)
+RGB_MATRIX_EFFECT(LINES)
 
 #ifdef RGB_MATRIX_CUSTOM_EFFECT_IMPLS
 
 
-static bool TWO_FACES(effect_params_t* params) {
+static bool LINES(effect_params_t* params) {
     RGB_MATRIX_USE_LIMITS(led_min, led_max);
-    uint8_t  huedelta = 18;
+    uint8_t  huedelta = 28;
     HSV      hsv      = rgb_matrix_config.hsv;
     uint16_t time     = scale16by8(g_rgb_timer, rgb_matrix_config.speed / 8);
     hsv.h             = hsv.h + scale8(abs8(sin8(time) - 128) * 2, huedelta);
@@ -16,10 +16,13 @@ static bool TWO_FACES(effect_params_t* params) {
         if(i < 6 || (i > 22 && i < 29)) { // Underglow
             rgb_matrix_set_color(i, rgb.b, rgb.r, rgb.g);
         } else { // Keys
-            if(i%2 == 0) {
+            if((i > 43) || (i > 20 && i < 23)) { // Thumbs
+                rgb_matrix_set_color(i, rgb.b, rgb.r, rgb.g);
+            }
+            else if((i > 28 && (i + 1)%10 > 4) || (i < 21 && (i - 1)%10 > 4)) { // Line even
                 rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
-            }else{
-                rgb_matrix_set_color(i, rgb.r, rgb.b, rgb.g);
+            }else{ // Line odd
+                rgb_matrix_set_color(i, rgb.g, rgb.b, rgb.r);
             }
         }
     }
